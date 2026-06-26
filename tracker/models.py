2a79b9ea4e3b397f98c0f5379d2571f717eea2e6
Zinchuk <ipz251_zzm@student.ztu.edu.ua>
2026-06-26 14:09:02 +0300

    Add superuser and configure admin panel

--- tracker/models.py
+++ tracker/models.py
@@ -4,7 +4,7 @@ class Product(models.Model):
     name = models.CharField(max_length=256, verbose_name="Назва товару")
     url = models.URLField(unique=True, verbose_name="Посилання на товар")
     currentPrice = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Поточна ціна")
-    desiredPrice = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Поточна ціна")
+    desiredPrice = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True, verbose_name="Бажана ціна")
     lastChecked = models.DateTimeField(auto_now=True, verbose_name="Останнє оновлення")
 
     def __str__(self):
